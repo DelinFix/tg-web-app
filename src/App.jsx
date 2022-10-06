c16ede942bd744ba89490c1dfd40db851566e8cd
Денис Басенко <d.basenko@brightlab.me>
2022-10-06 16:18:02 +0300

    Add toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useEffect } from 'react'
 import './App.css'
+import Button from './components/Button/Button'
 import Header from './components/Header/Header'
+import { useTelegram } from './hooks/useTelegram'
 
 function App() {
+  const {tg, onToggleButton} = useTelegram()
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
 
   return (
     <div className="App">
       <Header />
+      <Button onClick={onToggleButton}>Togle</Button>
     </div>
   )
 }
